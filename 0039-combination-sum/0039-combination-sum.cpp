df0f65d8923387b54ee007d6be3b9e92bb7fd7c8
Ayush-y24 <ayushyedestudy24@gmail.com>
2026-09-05 22:49:40 +0530

    Time: 1 ms (63.01%), Space: 14.1 MB (76.48%) - LeetHub

---
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -0,0 +1,26 @@
+class Solution {
+    void f(int ind,int target,vector<int> &arr,vector<vector<int>> &ans,vector<int> &ds){
+        if(target==0){
+            ans.push_back(ds);
+            return;
+        }
+        if(ind>=arr.size()){
+            return;
+        }
+
+        if(arr[ind]<=target){
+            ds.push_back(arr[ind]);
+            f(ind,target-arr[ind],arr,ans,ds);
+            ds.pop_back();
+
+        }
+        f(ind+1,target,arr,ans,ds);
+    }
+public:
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+      vector<vector<int>>ans;
+      vector<int>ds;
+      f(0,target,candidates,ans,ds);
+      return ans;  
+    }
+};
