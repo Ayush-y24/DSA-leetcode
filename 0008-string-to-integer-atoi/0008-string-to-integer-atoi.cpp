ac827521655ab9dd2e444cf52afbb5e0ae3c92ac
Ayush-y24 <ayushyedestudy24@gmail.com>
2026-09-18 23:20:15 +0530

    Time: 0 ms (100%), Space: 9 MB (98.1%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
@@ -1,35 +1,45 @@
 class Solution {
 public:
     int myAtoi(string s) {
-
         int i = 0;
         int n = s.size();
-
-        // Skip spaces
-        while (i < n && s[i] == ' ')
-            i++;
-
-        // Sign
-        int sign = 1;
-        if (i < n && (s[i] == '+' || s[i] == '-')) {
-            if (s[i] == '-') sign = -1;
+        while(i < n && s[i] == ' '){
             i++;
         }
 
+        if (i == n) {
+            return 0;
+        }
+
+        bool pos = true;
+        if(s[i] == '+'){
+            i++;
+        }else if(s[i] == '-'){
+            pos = false;
+            i++;
+        }
+
+        while (i < n && s[i] == '0')
+            i++;
+
+    
         long long num = 0;
+        while(i < n && isdigit(s[i])){
+            num = num*10 + (s[i]-'0');
 
-        // Read digits
-        while (i < n && isdigit(s[i])) {
+            if(pos == true && num > INT_MAX){
+                return INT_MAX;
+            }
 
-            num = num * 10 + (s[i] - '0');
-
-            // Overflow
-            if (sign * num >= INT_MAX) return INT_MAX;
-            if (sign * num <= INT_MIN) return INT_MIN;
-
+            if(pos == false && -num < INT_MIN){
+                return INT_MIN;
+            }
             i++;
         }
 
-        return sign * num;
+        if(pos == false){
+            return -num;
+        }
+        return num;
     }
 };
